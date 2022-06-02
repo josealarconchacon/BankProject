13d20daf510d20b0179acb42524b8e0236da0ce4
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-06-02 12:35:34 -0400

    refactor client class

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,76 +1,65 @@
 package com.company;
 
 public class Client {
-    private String FirstName, LastName, Address;
-    private String email, PhoneNumber;
-    private int Ssn;
+    private String FirstName, LastName, Address, email;
+    private int SSN, PhoneNumber;
 
     public Client() {
-        FirstName = LastName = Address = email = PhoneNumber = "";
-        Ssn = 0;
+        this("First Name", "Last Name", "129 default st" ,"default@gmail.com", 0000000000, 00000000);
+        System.out.println("Empty Constructor called.");
     }
 
-    public Client(String DoB, String f_name, String l_name, String address, String phoneNumber, int ssn) {
+    public Client(String DoB, String f_name, String l_name, String address, int phoneNumber, int ssn) {
         this.FirstName = f_name;
         this.LastName = l_name;
         this.Address = address;
         this.PhoneNumber = phoneNumber;
-        this.Ssn = ssn;
-    }
-
-    public String getFirstName() {
-        return FirstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.FirstName = firstName;
-    }
-
-    public String getLastName() {
-        return LastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.LastName = lastName;
-    }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String address) {
-        this.Address = address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return PhoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.PhoneNumber = phoneNumber;
-    }
-
-    public int getSsn() {
-        return Ssn;
-    }
-
-    public void setSsn(int ssn) {
-        this.Ssn = ssn;
+        this.SSN = ssn;
     }
 
     public boolean equals(Client client) {
         if(super.equals(client) && this.FirstName.equals(client.FirstName) && this.LastName.equals(client.LastName)
-            && this.Address.equals(client.Address) && this.PhoneNumber.equals(client.PhoneNumber) && this.Ssn == client.Ssn) {
+            && this.Address.equals(client.Address) && this.PhoneNumber == client.PhoneNumber && this.SSN == client.SSN) {
             return true;
         }
         return false;
+    }
+
+    // getters and setters
+    public String getFirstName() {
+        return FirstName;
+    }
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getSSN() {
+        return SSN;
+    }
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        PhoneNumber = phoneNumber;
     }
 }
