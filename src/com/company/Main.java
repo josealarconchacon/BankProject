27a297d75299691f382450a790ef9064306b1d40
josealarconchacon <jose.alarcon.chacon@gmail.com>
2022-06-06 12:07:09 -0400

    test bank account class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    TestingDateClass();
+    public static void main(String[] args) throws Exception {
+//	    TestingDateClass();
+        TestBankA();
     }
-    public  static void TestingDateClass() {
+    public static void TestingDateClass() {
         Date D = new Date();
         D.setYear(2004);
         D.setMonth(2);
@@ -17,5 +18,12 @@ public class Main {
         System.out.println("D2: " + D2);
         Date D3 = new Date("1245-10-22");
         System.out.println("D3: " + D3);
+    }
+
+    public static void TestBankA() throws Exception {
+        BankAccount B = new BankAccount();
+        System.out.println("Default Bank: " + B);
+        B.deposit(100000.00);
+        System.out.println("Your Bank Account has a total of: " + B);
     }
 }
